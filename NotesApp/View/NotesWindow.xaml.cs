87494514cf19274ddafe11f960e30e4a1f94ab3b
Viktar Tolstsik <viktor.tolstik.96@gmail.com>
2023-02-06 12:13:08 +0300

    Toolbar implementation started

--- NotesApp/View/NotesWindow.xaml.cs
+++ NotesApp/View/NotesWindow.xaml.cs
@@ -28,5 +28,10 @@ namespace NotesApp.View
         {
             Application.Current.Shutdown();
         }
+
+        private void SpeechButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
