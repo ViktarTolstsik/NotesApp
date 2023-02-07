03c409610d403096d9a9dfb8872110d8e48f73d1
Viktar Tolstsik <viktor.tolstik.96@gmail.com>
2023-02-07 09:14:45 +0300

    Notebooks and notes displaying implemented

--- NotesApp/ViewModel/NotesVM.cs
+++ NotesApp/ViewModel/NotesVM.cs
@@ -4,45 +4,55 @@ using NotesApp.ViewModel.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NotesApp.ViewModel
 {
-	public class NotesVM
+	public class NotesVM : INotifyPropertyChanged
 	{
 		public ObservableCollection<Notebook> Notebooks { get; set; }
 		private Notebook selectedNotebook;
 
-		public Notebook SelectedNotebook
+        public Notebook SelectedNotebook
 		{
 			get { return selectedNotebook; }
 			set
 			{
 				selectedNotebook = value;
-				//TODO get the notes
+				OnPropertyChanged("SelectedNotebook");
+				GetNotes();
 			}
 		}
 		public ObservableCollection<Note> Notes { get; set; }
 		public NewNotebookCommand NewNotebookCommand { get; set; }
 		public NewNoteCommand NewNoteCommand { get; set; }
 
-		public NotesVM()
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public NotesVM()
 		{
 			NewNoteCommand = new NewNoteCommand(this);
 			NewNotebookCommand = new NewNotebookCommand(this);
+
+			Notebooks = new ObservableCollection<Notebook>();
+			Notes = new ObservableCollection<Note>();
+
+			GetNotebooks();
 		}
 
         public void CreateNotebook()
         {
             Notebook newNotebook = new Notebook()
             {
-                Name = "New notebook",
-
+                Name = "New notebook"
             };
 
             DatabaseHelper.Insert(newNotebook);
+
+			GetNotebooks();
         }
 
         public void CreateNote(int notebookId)
@@ -52,10 +62,41 @@ namespace NotesApp.ViewModel
 				NotebookId = notebookId,
 				CreatedAt = DateTime.Now,
 				UpdatedAt = DateTime.Now,
-				Title = "New note"
+				Title = $"New note for {DateTime.Now.ToString()}"
 			};
 
 			DatabaseHelper.Insert(newNote);
+
+			GetNotes();
 		}
-	}
+
+		private void GetNotebooks()
+		{
+			var notebooks = DatabaseHelper.Read<Notebook>();
+
+			Notebooks.Clear();
+			foreach(var notebook in notebooks) 
+			{
+				Notebooks.Add(notebook);
+			}
+		}
+
+        private void GetNotes()
+        {
+			if (SelectedNotebook != null)
+			{
+				var notes = DatabaseHelper.Read<Note>().Where(n => n.NotebookId == SelectedNotebook.Id).ToList();
+
+				Notes.Clear();
+				foreach (var note in notes)
+				{
+					Notes.Add(note);
+				}
+			}
+        }
+		private void OnPropertyChanged(string propertyName) 
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+    }
 }
