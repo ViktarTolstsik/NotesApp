d4aa10558c359dcfd9f292375b2c6e4afb4669d8
Viktar Tolstsik <viktor.tolstik.96@gmail.com>
2023-02-21 10:38:24 +0300

    Firebase database data updating and deleting functionality implemented

--- NotesApp/ViewModel/Helpers/DatabaseHelper.cs
+++ NotesApp/ViewModel/Helpers/DatabaseHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using NotesApp.Model;
 using SQLite;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace NotesApp.ViewModel.Helpers
 {
@@ -49,40 +51,69 @@ namespace NotesApp.ViewModel.Helpers
                 }
             }
         }
-        public static bool Update<T>(T item)
+        public static async Task<bool> Update<T>(T item) where T : HasId
         {
-            bool result = false;
+            //bool result = false;
 
-            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            //using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            //{
+            //    conn.CreateTable<T>();
+            //    int rows = conn.Update(item);
+            //    if (rows > 0)
+            //    {
+            //        result = true;
+            //    }
+            //}
+
+            //return result;
+            var jsonBody = JsonConvert.SerializeObject(item);
+            var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            using (var client = new HttpClient())
             {
-                conn.CreateTable<T>();
-                int rows = conn.Update(item);
-                if (rows > 0)
+                var result = await client.PatchAsync($"{dbPath}{item.GetType().Name.ToLower()}/{item.Id}.json", content);
+
+                if (result.IsSuccessStatusCode)
                 {
-                    result = true;
+                    return true;
+                }
+                else
+                {
+                    return false;
                 }
             }
-
-            return result;
         }
-        public static bool Delete<T>(T item)
+        public static async Task<bool> Delete<T>(T item) where T : HasId
         {
-            bool result = false;
+            //bool result = false;
 
-            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            //using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            //{
+            //    conn.CreateTable<T>();
+            //    int rows = conn.Delete(item);
+            //    if (rows > 0)
+            //    {
+            //        result = true;
+            //    }
+            //}
+
+            //return result;
+            using (var client = new HttpClient())
             {
-                conn.CreateTable<T>();
-                int rows = conn.Delete(item);
-                if (rows > 0)
+                var result = await client.DeleteAsync($"{dbPath}{item.GetType().Name.ToLower()}/{item.Id}.json");
+
+                if (result.IsSuccessStatusCode)
                 {
-                    result = true;
+                    return true;
+                }
+                else
+                {
+                    return false;
                 }
             }
-
-            return result;
         }
 
-        public static List<T> Read<T>() where T : new() //Parameterless constructor
+        public static async Task<List<T>> Read<T>() where T : HasId
         {
             //List<T> items;
 
@@ -93,7 +124,35 @@ namespace NotesApp.ViewModel.Helpers
             //}
 
             //return items;
-            using (var client = new Http)
+            List<T> list = new List<T>();
+
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync($"{dbPath}{typeof(T).Name.ToLower()}.json");
+                var jsonResult = await result.Content.ReadAsStringAsync();
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var objects = JsonConvert.DeserializeObject<Dictionary<string, T>>(jsonResult);
+
+                    if (objects == null)
+                    {
+                        return list;
+                    }
+
+                    foreach(var o in objects)
+                    {
+                        o.Value.Id = o.Key;
+                        list.Add(o.Value);
+                    }
+
+                    return list;
+                }
+                else 
+                {
+                    return null;
+                }
+            }
         }
     }
 }
