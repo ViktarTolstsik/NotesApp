520c0e9bd675ba0883bb422c6a263b29fbcd0fff
Viktar Tolstsik <viktor.tolstik.96@gmail.com>
2023-02-23 11:58:32 +0300

    UI improvement continued

--- NotesApp/View/LoginWindow.xaml.cs
+++ NotesApp/View/LoginWindow.xaml.cs
@@ -42,5 +42,15 @@ namespace NotesApp.View
                 DragMove();
             }
         }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
